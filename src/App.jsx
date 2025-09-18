35178dec7b385c07a3e64d6712303d178a1008af
Beamlak Tessema <beamlaktessemak@gmail.com>
2025-09-18 11:49:58 +0300

    carousel added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import LowerHeader from './Components/Header/LowerHeader'
+import Carousel from './Components/Carasol/Carousel'
 
 const App = () => {
   return (
     <>
     <Header/>
     <LowerHeader/>
+    <Carousel/>
     </>
   )
 }
